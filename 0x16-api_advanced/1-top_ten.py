1bb0eed1d990c3df38aa67d261d94bf7e00cf554
IbnuJabir <kedirjabir12@gmail.com>
2024-06-20 02:11:31 +0300

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""This python module contains a script that interacts with the Reddit API"""
-from requests import get
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
 
 
 def top_ten(subreddit):
-    """Queries the reddit api and prints the titles of the first 10
-    hot posts."""
-    param = {"limit": 10}
-    header = {"User-Agent": "0x16.api.advanced.project:eleccrazy"}
-    res = get("https://www.reddit.com/r/{}/hot/.json".format(subreddit),
-              headers=header, allow_redirects=False, params=param)
-    if res.status_code == 404:
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
-    else:
-        for r in res.json().get("data").get("children"):
-            print(r.get("data").get("title"))
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
