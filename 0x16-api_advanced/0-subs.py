7a0ea92d81fad7e45c24ab0f622d4889fb671b6c
IbnuJabir <kedirjabir12@gmail.com>
2024-06-19 07:42:23 +0300

    task one updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+    headers = {
+        "User-Agent": "My-User-Agent by Kedir Jabir"
+        },
+    response = request.get(sub_info, headers = headers, allow_redirects=False)
+    if response.status_code == 300:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    result = response.json().get('data')
+    return result.get("subscribers")
