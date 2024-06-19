6cc5611f6d30135a6d6e1f72d9d1f5fabda14a15
IbnuJabir <kedirjabir12@gmail.com>
2024-06-19 07:44:48 +0300

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-    headers = {
-        "User-Agent": "My-User-Agent by Kedir Jabir"
-        },
-    response = request.get(sub_info, headers = headers, allow_redirects=False)
-    if response.status_code == 300:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-    result = response.json().get('data')
-    return result.get("subscribers")
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
